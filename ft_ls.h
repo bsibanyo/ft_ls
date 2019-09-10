e597b9a6da08e187108e6912fa4ac882a79744bf
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-09-10 13:22:19 +0200

    ft_ls.h

--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 09:41:33 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/09/09 12:02:40 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/09/10 13:21:56 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,76 @@
 #include "./libft/libft.h"
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <errno.h>
+#include <pwd.h>
+#include <fcntl.h>
+
 
 //#include "ft_list_dir.c"
 
-typedef struct		m_list						//typedef declaration
+typedef		int t_bool
+
+typedef 	struct s_files
 {
-	void			*content;
-	size_t			content_size;
-	struct s_list	*next;
-}					d_list;
+	char 			*name;
+	char			*link;
+	struct			s_files *next;
+	struct			s_files *preview;
+	struct			s_files *sub_dir;
+	struct			stat  stat;
+	time_t			mtime;
+	time_t			atime;
 
-void       ft_list_dir(char *path);
+}					t_files
 
+typedef		struct s_flags
+{
+	t_bool			a;
+	t_bool			l;
+	t_bool			R;
+	t_bool			r;
+	t_bool			t;
+
+}					t_flags
+
+typedef		struct	s_r
+{
+	DIR				*ds;
+	struct	dirent 	*d;
+	t_files			*files;
+
+}					t_r
+
+typedef 	struct	s_start
+{
+	int		start;
+	int		selected;
+
+}					t_start
+
+typedef		struct	s_list
+{
+	t_files			*files;
+	t_files			tmp;
+	t_files			tmp2;
+}					t_list
+
+typedef		struct	s_single
+{
+	t_files			*alist;
+	struct	stat  	fstat;
+	char			*nw_path;
+
+}					t_single
+
+
+typedef		struct	s_main
+{
+	t_flags			cflags;
+	int 			start;
+	char			*pwd;
+}					t_main
 
 #endif
