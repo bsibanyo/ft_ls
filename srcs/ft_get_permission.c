710d7a33266b3914ec94b8611705754fc7f2ce9a
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-09-12 11:13:13 +0200

    srcs

--- srcs/ft_get_permission.c
+++ srcs/ft_get_permission.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 13:28:16 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/09/10 13:29:31 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/09/12 10:09:04 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,36 @@ void		ft_group_permission(struct stat st)
 		ft_putchar("-");
 }
 
+void		ft_other_permission(struct stat st)
+{
+	mode_t	permission;
+
+	permission = st.st_mode & S_IRWXOG;
+	if (permission & S_IROTH)
+		ft_putchar("r");
+	else
+		ft_putchar("-");
+	if (permission & S_IWOTH)
+		ft_putchar("w");
+	else
+		ft_putchar("-");
+	if (permission & S_IXOTH)
+		ft_putchar("x");
+	else
+		ft_putchar("-");
+}
+
+
 void		ft_isdir(struct stat st)
 {
 	if (S_ISDIR(st.st_mode))
 		ft_putchar("d");
+}
+
+void	ft_print_permission(struct stat st)
+{
+	ft_isdir(st);
+	ft_get_user_permission(st);
+	ft_group_permission(st);
+	ft_other_permission(st);
 }
