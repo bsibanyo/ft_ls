8699e1c72c8b7e75c24d38743a69fa96b752f6ec
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-09-12 11:15:41 +0200

    srcs

---
+++ srcs/modify.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   modify.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/12 11:06:59 by bsibanyo          #+#    #+#             */
+/*   Updated: 2019/09/12 11:09:22 by bsibanyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_ls.h"
+
+void	sort_time(t_files *curr, t_files *node)
+{
+	if (ft_strcmp(curr->next->name, node->name) > 0)
+	{
+		node->next = curr->next;
+		curr->next = node->next; // print time when it was modified
+	}
+	else
+	{
+		while (curr->next != NULL &&
+				curr->next->mtime - node->mtime == 0)
+		{
+			curr = curr->next;
+			node->next = curr->next;
+			curr = node->next;
+		}
+	}
+}
+
+void	sort_time_b(t_files **stack, t_files new_node)
+{
+	t_files *curr;
+
+	if (*stack == NULL || (*stack)->mtime - new_node->mtime < 0)
+	{
+		new_node->next = *stack;
+		*stack = new_node;
+	}
+	else
+	{
+		curr = *stack;
+		while (curr->next != NULL &&
+				curr->next->mtime - new_node->mtime > 0)
+			curr = curr->next;
+		if (curr->next != NULL &&
+				curr->next->mtime - new_node->mtime == 0)
+			sort_time(curr, new_node);
+		else
+		{
+			curr = curr->next;
+			node->next = curr->next;
+			curr = node->next;
+		}
+}
