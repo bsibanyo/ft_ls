7b9b20114e64ff4a72bfb9aecc868024b25e9566
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-09-25 12:07:20 +0200

    exit.c

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,12 +6,11 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/13 15:46:31 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/09/13 15:46:35 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/09/25 12:06:57 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_ls.h"
-//#include "ft_list_dir.c"
 
 void	ft_exit(const char *message)
 {
